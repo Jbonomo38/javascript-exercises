04fbed4007e208b54ae7551253aad73233f19d46
Jack <jbonomo38@gmail.com>
2024-02-22 23:08:43 -0500

    Solve 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,8 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...removeVals) {
+    return array.filter((val) => !removeVals.includes(val));
 };
+
+removeFromArray([1, 2, 3, 4], 3, 2)
 
 // Do not edit below this line
 module.exports = removeFromArray;
