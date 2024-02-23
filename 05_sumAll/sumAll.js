96ae6d3d7768ab9eb591e4bd09bbc3fdc9d991c8
Jack <jbonomo38@gmail.com>
2024-02-22 23:24:03 -0500

    Solve 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
+const sumAll = function(numOne, numTwo) {
+    if(!Number.isInteger(numOne) || !Number.isInteger(numTwo)) return 'ERROR';
+    
+    let max = Math.max(numOne, numTwo);
+    let min = Math.min(numOne, numTwo);
 
+    if(min < 0 || max < 0) return 'ERROR';
+    
+    if(min + 1 === max) {
+        return min + max;
+    }
+    return min + sumAll(min + 1, max);
 };
 
 // Do not edit below this line
