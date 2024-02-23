ac104524340b2398a89e09498fafbbd268a863e3
Jack <jbonomo38@gmail.com>
2024-02-22 22:02:16 -0500

    Solve problem 01, 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(string, num) {    
+    if(string.length == 0) return '';
+    if(num < 0) return 'ERROR';
+    
+    let returnString = '';
 
+    for(let i = 0; i < num; i ++) {
+        returnString += string;
+    }
+
+    return returnString;
 };
 
 // Do not edit below this line
