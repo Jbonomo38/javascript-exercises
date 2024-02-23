1b239bfea1228b864ad0e546c220dd5ba36349cf
Jack <jbonomo38@gmail.com>
2024-02-22 22:18:53 -0500

    Solve 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,18 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let returnString = '';
+    let indexNum = string.length;
 
+    while(indexNum >= 0) {
+        returnString += string.charAt(indexNum);
+        indexNum--;
+    }
+
+    console.log(returnString);
+
+    return returnString;
 };
+
+reverseString("sample");
 
 // Do not edit below this line
 module.exports = reverseString;
