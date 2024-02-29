38840c80dc7ad8614bb9996642b1136faa1b91e6
Jack <jbonomo38@gmail.com>
2024-02-28 21:11:39 -0500

    Solve 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,28 @@
-const add = function() {
-	
+const add = function(numOne, numTwo) {
+    return numOne + numTwo;
 };
 
-const subtract = function() {
-	
+const subtract = function(numOne, numTwo) {
+    return numOne - numTwo;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+    return nums.reduce((total, currentItem) => total + currentItem, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+    return nums.reduce((result, currentItem) => currentItem * result);
 };
 
-const power = function() {
-	
+const power = function(num, pow) {
+    if(pow === 1) return num;
+    return num * power(num, pow - 1);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if(num === 0) return 1;
+  if(num === 1) return num;
+    return factorial(num - 1) * num;
 };
 
 // Do not edit below this line
