5e82554dba820500e010a3e14ec64357fc5b70b9
Jack <jbonomo38@gmail.com>
2024-02-28 22:48:20 -0500

    Solve 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    let sortedArray = array.sort((element1, element2) => {
+        const currentYear = new Date().getFullYear();
+        const deathYear1 = element1.yearOfDeath || currentYear;
+        const deathYear2 = element2.yearOfDeath || currentYear;
+        const age1 = deathYear1 - element1.yearOfBirth;
+        const age2 = deathYear2 - element2.yearOfBirth;
+        return age2 - age1;
+    });
+    console.log(sortedArray);
 
+    return sortedArray[0];
 };
+
+function getAge(person) {
+    return person.yearOfDeath - person.yearOfBirth
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
